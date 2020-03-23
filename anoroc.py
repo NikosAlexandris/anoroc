be15d6222611febe8dc2c5e9a5b913b9d3dfd62e
Nikos Alexandris <nik@nikosalexandris.net>
2020-03-23 12:59:00 +0100

    Update formatting for code legibility and minimise number of future commits
    Arguably formatting is often just "personal taste".
    Best to use a formatter such as https://github.com/psf/black

--- anoroc.py
+++ anoroc.py
@@ -20,24 +20,39 @@ if __name__ == "__main__":
     if args.country:
         country = args.country
         plot_countries([str(country)], title=country)
+
     elif args.region and args.exclude:
         region_name = args.region
+        countries = get_countries(coutries_filename="countries.yaml")
         region, excluded = region(
-            region_name, countries, data_confirmed, exclude=[str(args.exclude)]
-        )
+                                region_name,
+                                countries,
+                                data_confirmed,
+                                exclude=[str(args.exclude)],
+                           )
         if region and excluded:
-            plot_countries(region, title=region_name + " excluding " + args.exclude)
+            plot_countries(
+                    region,
+                    title=region_name + " excluding " + args.exclude,
+            )
         elif region:
             print(args.exclude + " not in " + args.region)
             print("Plotting data for the whole region.")
             plot_countries(region, title=region_name)
+
     elif args.region:
         region_name = args.region
-        region, _ = region(region_name, countries, data_confirmed)
+        region, _ = region(
+                        region_name,
+                        countries,
+                        data_confirmed,
+                    )
         if region:
             plot_countries(region, title=region_name)
+
     elif args.update:
         print("\n\nFresh data downloaded.")
+
     else:
         print("\nNo country selected. Plotting data for whole world.")
         world, _ = region("World", countries, data_confirmed)
