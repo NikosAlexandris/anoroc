3db35f36868f87eb3f9e9044fe514fd81770ccd1
Nikos Alexandris <nik@nikosalexandris.net>
2020-03-23 12:55:29 +0100

    Move constants to constants.py
    By mistake, removal of lines of concern in anoroc.py committed in bdf2d87

---
+++ constants.py
@@ -0,0 +1,7 @@
+URL_CONFIRMED = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+URL_DEATH = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
+URL_RECOVERED = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
+FILENAME1 = "cases_confirmed.csv"
+FILENAME2 = "cases_death.csv"
+FILENAME3 = "cases_recovered.csv"
+
